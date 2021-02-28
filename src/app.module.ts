b08f796a20c5e79a6e09b479dc68892c75e94cc4
Farid Safwan <faridsafwan@aerodyne.co>
2021-03-01 07:39:43 +0800

    build(init): Initial commit

---
+++ src/app.module.ts
@@ -0,0 +1,16 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ItemModule } from './item/item.module';
+import { MongooseModule } from '@nestjs/mongoose';
+
+@Module({
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost/nest'),
+    //  { useNewUrlParser: true }
+    ItemModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
