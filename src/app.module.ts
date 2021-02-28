8b7b75d0678790d9a22d7af74b0a570d76301e5e
Farid Safwan <faridsafwan@aerodyne.co>
2021-03-01 07:54:40 +0800

    refactor(item module): Change file location

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ItemModule } from './item/item.module';
+import { ItemModule } from './modules/item/item.module';
 import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
